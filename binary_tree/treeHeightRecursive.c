d8388c1b61e8c1f76b8f4f8fede71672f67a889a
Casey9527 <casey9527@yahoo.com>
2017-01-18 10:08:13 -0800

    Create treeHeightRecursive.c

---
+++ binary_tree/treeHeightRecursive.c
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX(a, b) (a < b ? b : a)
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+typedef struct node node;
+
+node* newNode(int data) {
+  node* new_node = (node *) malloc(sizeof(node));
+  new_node->data = data;
+  new_node->left = NULL;
+  new_node->right = NULL;
+ 
+  return(new_node);
+}
+
+// DFS
+int getHeight(node *root) {
+    if (!root) {
+        return 0;   
+    }    
+    return MAX(getHeight(root->left), getHeight(root->right)) + 1;
+}
+
+int main()
+{
+    struct node *root = newNode(1);
+ 
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5); 
+   
+    printf("Hight of tree is %d", getHeight(root));
+   
+    getchar();
+    return 0;
+}
