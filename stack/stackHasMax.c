16ddc1a912ae23e6b03c46009c817254c463c6bc
Casey9527 <casey9527@yahoo.com>
2017-01-19 15:40:29 -0800

    Update stackHasMax.c
    add more comments.

--- stack/stackHasMax.c
+++ stack/stackHasMax.c
@@ -1,14 +1,21 @@
+/**
+ *   implement a stack that can do push(), pop(), max() in O(1) time. 
+ *   stack is implemented using linked list
+ *   max() should return the node (not value) containing max value;
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+/* linked list node definition */
 typedef struct Node {
     int key;
     struct Node *next;
     struct Node *max;
 } Node;
 
-Node* newNode(int data)
+/* create node dynamically */
+Node* createNode(int data)
 {
     Node *tmp = (Node *) malloc(sizeof(Node));
     tmp->key = data;
@@ -16,13 +23,15 @@ Node* newNode(int data)
     return (tmp);
 }
 
+/* stack implemented by linked list */
 typedef struct Stack {
     Node *top;
     int count;
     int capacity;
 } Stack;
 
-Stack *newStack(int cap) {
+/* create stack dynamically */
+Stack *createStack(int cap) {
     Stack *s = (Stack *) malloc(sizeof(Stack));
     s->top = NULL;
     s->count = 0;
@@ -40,13 +49,14 @@ int isFull(Stack *s)
     return s->count == s->capacity;
 }
 
-void push(Stack *s, Node *node)
+void push(Stack *s, int val)
 {
+    Node *node = createNode(val);
     if (isFull(s)) {
         printf("stack full\n");
         return;
     }
-    // link    
+    // link
     node->next = s->top;
     // set max
     if (s->top == NULL || 
@@ -61,18 +71,31 @@ void push(Stack *s, Node *node)
     s->count++;
 }
 
-Node* pop(Stack *s)
+/* pop the top (free it and return its key value) */
+int pop(Stack *s)
+{
+    if (isEmpty(s)) {
+        printf("stack empty\n");
+        return -1;
+    }
+    Node *old_top = s->top;
+    int result = old_top->key;
+    s->top = s->top->next;
+    s->count--;
+    free(old_top);
+    return result;
+}
+
+Node* peek(Stack *s)
 {
     if (isEmpty(s)) {
         printf("stack empty\n");
         return NULL;
     }
-    Node *result = s->top;
-    s->top = s->top->next;
-    s->count--;
-    return result;
+    return s->top;
 }
 
+/* return the node which has maximum key value */
 Node* max(Stack *s)
 {
     if (isEmpty(s)) {
@@ -84,70 +107,24 @@ Node* max(Stack *s)
 
 int main()
 {
-    Node *node1 = newNode(1);
-    Node *node2 = newNode(2);
-    Node *node3 = newNode(3);
-    Node *node4 = newNode(3);
-    Node *node5 = newNode(4);
-    Node *node6 = newNode(9);
-    Node *node7 = newNode(5);
-    Node *node8 = newNode(6);
-    Node *node9 = newNode(2);
-    
-    
-    Stack *s = newStack(32);
-    push(s, node1);
-    push(s, node2);
-    push(s, node3);
-    push(s, node4);
-    push(s, node5);
-    push(s, node6);
-    push(s, node7);
-    push(s, node8);
-    push(s, node9);
-    
-    Node *tmp;
-    
-    printf("max: %d\n", max(s)->key);
-    tmp = pop(s);
-    printf("pop: %d\n-------------------\n", tmp->key);
-    
-    
-    printf("max: %d\n", max(s)->key);
-    tmp = pop(s);
-    printf("pop: %d\n-------------------\n", tmp->key);    
-    
-    printf("max: %d\n", max(s)->key);
-    tmp = pop(s);
-    printf("pop: %d\n-------------------\n", tmp->key);
+    int array[] = {1, 2, 3, 3, 4, 9, 5, 6, 2};
+    int size = sizeof(array) / sizeof(array[0]);
+    /* create a stack */
+    Stack *s = createStack(32);
+    int i;
 
+    printf("Pushing...:\n");
+    for (i = 0; i < size; i ++) {
+        printf("PUSH: %d\n", array[i]);
+        push(s, array[i]);
+        printf("MAX: %d\n", max(s)->key);
+    }
 
-    printf("max: %d\n", max(s)->key);
-    tmp = pop(s);
-    printf("pop: %d\n-------------------\n", tmp->key);
-
-    printf("max: %d\n", max(s)->key);
-    tmp = pop(s);
-    printf("pop: %d\n-------------------\n", tmp->key);
-
-    printf("max: %d\n", max(s)->key);
-    tmp = pop(s);
-    printf("pop: %d\n-------------------\n", tmp->key);
-    
-    
-    printf("max: %d\n", max(s)->key);
-    tmp = pop(s);
-    printf("pop: %d\n-------------------\n", tmp->key);
-    
-    printf("max: %d\n", max(s)->key);
-    tmp = pop(s);
-    printf("pop: %d\n-------------------\n", tmp->key);
-
-    printf("max: %d\n", max(s)->key);
-    tmp = pop(s);
-    printf("pop: %d\n-------------------\n", tmp->key);
-    
+    printf("\nPoping...:\n");
+    for (i = 0; i < size; ++i) {
+        printf("MAX:  %d\n", max(s)->key);
+        printf("POP:  %d\n", pop(s));
+    }
     return 0;
 }
-
 
