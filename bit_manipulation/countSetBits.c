a31af315a7ba91b40a083250c2430a69291f6583
Casey9527 <casey9527@yahoo.com>
2017-01-30 23:47:03 -0800

    Update countSetBits.c

--- bit_manipulation/countSetBits.c
+++ bit_manipulation/countSetBits.c
@@ -2,9 +2,10 @@
 
 int countSetBits(int n) {
     unsigned int count = 0;
-    while (n) {
-        n &= (n - 1);
-        count++;
+    while (n) {         /* non-zero: there exists a rightmost set bit */
+        count ++;       
+        n &= (n - 1);   /* unset the rightmost set bit 
+                           n-1 would toggle all bits from the rightmost set bit (included) to right end */
     }
     return count;
 }
