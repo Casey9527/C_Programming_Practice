1477b63bd868de00631470cdc2628fb9767e0ff6
Casey9527 <casey9527@yahoo.com>
2017-02-09 12:48:32 -0800

    Update bubbleSort01.c

--- sorting/bubbleSort01.c
+++ sorting/bubbleSort01.c
@@ -20,15 +20,18 @@ void bubbleSort(int a[], int n)
     int swapped;
     int i, j;
     
-    for (i = 0; i < n - 1; i++) {
-        swapped = 0;    // not swapped
-        for (j = 0; j < n - i - 1; j++) {
+    for (i = 0; i < n - 1; i++) {           // every iteration, we put a number at its correct position
+                                            // i.e. we put a big number at somewhere rightside
+                                            // i means # of items who already set on its correct position
+        
+        swapped = 0;                        // a flag to record whether we do swap in current iteration  
+        for (j = 0; j < n - i - 1; j++) {   // swap on the remaing items. index range from i+1 to n-i-2
             if (a[j] > a[j + 1]) {
-                swap(a, j, j + 1);
+                swap(a, j, j + 1);          
                 swapped = 1;
             }
         }
-        if (swapped == 0) {
+        if (swapped == 0) {                 // no swapping in current iteration. stops here!
             break;
         }
     }
