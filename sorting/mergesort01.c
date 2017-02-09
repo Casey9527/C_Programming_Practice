8e90c4fb799d56fff23f09af7f1ecf4ce64d773e
Casey9527 <casey9527@yahoo.com>
2017-02-09 12:38:36 -0800

    Update mergesort01.c

--- sorting/mergesort01.c
+++ sorting/mergesort01.c
@@ -2,14 +2,21 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * merge(): merge two sub-arrays
+ *
+ * arr:    starting position of the combined array
+ * m  :    the middle index
+ * n  :    length of the combined array
+ *
+ **/
 void merge(int *arr, int m, int n)
 {
     int *tmp = (int *) malloc(sizeof(int) * n);
     int i, j, k;
     for (k = 0, i = 0, j = m; k < n; k++) {
-        tmp[k]   = j == n ? arr[i++] 
-                 : i == m ? arr[j++]
+        tmp[k]   = j == n ? arr[i++]            // left sub-array has been walked over
+                 : i == m ? arr[j++]            // right sub-array has been walked over
                  : arr[i] < arr[j] ? arr[i++]
                  :                   arr[j++];
     }
@@ -20,14 +27,21 @@ void merge(int *arr, int m, int n)
     free(tmp);
 }
 
+/**
+ * mergesort(): do merge sort on array
+ *
+ * arr:    starting position of the array
+ * n  :    length of the array
+ *
+ **/
 void mergesort(int *arr, int n)
 {
     if (n < 2) {
         return;
     }
     int m = n / 2;
-    mergesort(arr, m);
-    mergesort(arr + m, n - m);
+    mergesort(arr, m);          // the left part: 0 ~ m-1
+    mergesort(arr + m, n - m);  // the right part: m ~ n-1
     merge(arr, m, n);
 }
 
