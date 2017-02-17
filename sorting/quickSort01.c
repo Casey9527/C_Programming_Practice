5249df1da7821fdd6e1a67b30f42675dcf34561c
Casey9527 <casey9527@yahoo.com>
2017-02-17 00:03:45 -0800

    Create quickSort01.c
    always choose the middle element as pivot

---
+++ sorting/quickSort01.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * always select the A[len/2] as pivot
+ *
+ * https://rosettacode.org/wiki/Sorting_algorithms/Quicksort#C
+ *
+ **/
+void quicksort(int *A, int len)
+{
+	if (len <= 1) {
+		return;
+	}
+
+	int pivot = A[len/2];
+
+	int i, j, tmp;
+
+	for (i = 0, j = len - 1; ; i ++, j --) {
+		while (A[i] < pivot)	i ++;
+		while (A[j] > pivot)	j --;
+
+		if (i >= j) break;
+
+		tmp  = A[i];
+		A[i] = A[j];
+		A[j] = tmp;
+	}
+
+	quicksort(A, i);
+	quicksort(A + i, len - i);
+}
+
+int main()
+{
+	int A[] = {1, 2, 3, 4, 5, 6};
+	int B[] = {6, 5, 4, 3, 2, 1};
+	int C[] = {2, 1, 3, 4, 6, 5};
+	int D[] = {4, 5, 6, 1, 2, 3};
+
+	quicksort(A, 6);
+	quicksort(B, 6);
+	quicksort(C, 6);
+	quicksort(D, 6);
+
+	int i;
+	printf("  A B C D\n");
+	for (i = 0; i < 6; i ++) {
+		printf("%d %d %d %d %d\n", i, A[i], B[i], C[i], D[i]);
+	}
+	return 0;
+}
